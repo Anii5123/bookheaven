890241fd99f69622468c8730295e508dd8813027
Anii5123 <anipatil5122003@gmail.com>
2024-09-17 17:01:14 +0530

    .env updated

--- conn/conn.js
+++ conn/conn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const conn = async () => {
     try {
-        await mongoose.connect(process.env.URI);
+        await mongoose.connect(process.env.MONGO_URI); // Use MONGO_URI instead of URI
         console.log("Connected to Database..");
     } catch (error) {
         console.log("Database connection failed:", error);
